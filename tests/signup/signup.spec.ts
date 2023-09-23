e9f44cbde9e00d90983146917c16be94e1bab61a
Ishaan Puniani <ishaan_puniani@yahoo.com>
2023-09-23 12:14:21 +0200

    Added recorded test

--- tests/signup/signup.spec.ts
+++ tests/signup/signup.spec.ts
@@ -7,6 +7,21 @@ test('Signup Page', async ({ page }) => {
   // Expect a title "to contain" a substring.
   await expect(page).toHaveScreenshot({ fullPage: true });
 });
+test('test invalid email', async ({ page }) => {
+  await page.goto('https://pulse-frontend.web.app/auth/signup');
+  await page.getByPlaceholder('First Name').click();
+  await page.getByPlaceholder('First Name').fill('ishaan');
+  await page.getByPlaceholder('Last Name').click();
+  await page.getByPlaceholder('Last Name').fill('puniani');
+  await page.locator('form').click();
+  await page.getByPlaceholder('Email').click();
+  await page.getByPlaceholder('Email').fill('test@');
+  await page.getByPlaceholder('Password').click();
+  await page.getByPlaceholder('Password').fill('123456');
+  await page.getByRole('button', { name: ' Sign Up with E-mail' }).click();
+  
+  await expect(page).toHaveScreenshot({ fullPage: true });
+});
 
 // test('get started link', async ({ page }) => {
 //   await page.goto('https://playwright.dev/');

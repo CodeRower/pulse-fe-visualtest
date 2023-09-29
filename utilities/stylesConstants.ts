95d20cb5f7010de953964dd9e00337fb2452e84a
RanaMahato <rana.mahato@coderower.com>
2023-09-29 22:30:39 +0530

    button style added

--- utilities/stylesConstants.ts
+++ utilities/stylesConstants.ts
@@ -1,21 +1,375 @@
 export const primaryButtonStyles = {
+  // chromium: {
+  //   backgroundColor: "rgb(43, 95, 71)",
+  //   borderRadius: "800px",
+  // },
+  // "chromium-ipad-landscape": {
+  //   backgroundColor: "rgb(43, 95, 71)",
+  //   borderRadius: "800px",
+  // },
+  // "chromium-ipad-portrait": {
+  //   backgroundColor: "rgb(43, 95, 71)",
+  //   borderRadius: "800px",
+  // },
+  // MobileChrome: {
+  //   backgroundColor: "rgb(43, 95, 71)",
+  //   borderRadius: "800px",
+  //   height : "36px",
+  //   fontSize : "16px" ,
+  // border :"0.5px solid #EAEFED"  },
+
   chromium: {
-    backgroundColor: "rgb(43, 95, 71)",
+    backgroundColor: "#2B5F47",
     borderRadius: "800px",
+    color: "#fff",
+    height: "64px",
+    fontSize: "20px",
+    fontWeight: "600",
+    padding : "20px 64px"
   },
   "chromium-ipad-landscape": {
-    backgroundColor: "rgb(43, 95, 71)",
+    backgroundColor: "#2B5F47",
     borderRadius: "800px",
+    color: "#fff",
+    height: "64px",
+    fontSize: "20px",
+    fontWeight: "600",
+    padding : "20px 64px"
   },
   "chromium-ipad-portrait": {
-    backgroundColor: "rgb(43, 95, 71)",
+    backgroundColor: "#2B5F47",
     borderRadius: "800px",
+    color: "#fff",
+    height: "64px",
+    fontSize: "20px",
+    fontWeight: "600",
+    padding : "20px 64px"
   },
   MobileChrome: {
-    backgroundColor: "rgb(43, 95, 71)",
+    backgroundColor: "#2B5F47",
     borderRadius: "800px",
+    color: "#fff",
+    height: "36px",
+    fontSize: "16px",
+    fontWeight: "600",
+    padding :"8px 16px"
   },
+
 };
+
+//for light button
+export const lightButtonStyles = {
+  chromium: {
+    backgroundColor: "#EAEFED",
+    borderRadius: "800px",
+    color: "#fff",
+    height: "64px",
+    fontSize: "20px",
+    fontWeight: "600",
+    padding : "20px 64px"
+  },
+  "chromium-ipad-landscape": {
+    backgroundColor: "#EAEFED",
+    borderRadius: "800px",
+    color: "#fff",
+    height: "64px",
+    fontSize: "20px",
+    fontWeight: "600",
+    padding : "20px 64px"
+  },
+  "chromium-ipad-portrait": {
+    backgroundColor: "#EAEFED",
+    borderRadius: "800px",
+    color: "#fff",
+    height: "64px",
+    fontSize: "20px",
+    fontWeight: "600",
+    padding : "20px 64px"
+  },
+  MobileChrome: {
+    backgroundColor: "#EAEFED",
+    borderRadius: "800px",
+    color: "#fff",
+    height: "36px",
+    fontSize: "16px",
+    fontWeight: "600",
+    padding : "8px 16px"
+  },
+  
+};
+
+//for border color
+export const borderButtonStyles = {
+  chromium: {
+    backgroundColor: "#2B5F47",
+    borderRadius: "800px",
+    color: "#fff",
+    height: "64px",
+    fontSize: "20px",
+    fontWeight: "600",
+    padding : "20px 64px"
+  },
+  "chromium-ipad-landscape": {
+    backgroundColor: "#2B5F47",
+    borderRadius: "800px",
+    color: "#fff",
+    height: "64px",
+    fontSize: "20px",
+    fontWeight: "600",
+    padding : "20px 64px"
+  },
+  "chromium-ipad-portrait": {
+    backgroundColor: "#2B5F47",
+    borderRadius: "800px",
+    color: "#fff",
+    height: "64px",
+    fontSize: "20px",
+    fontWeight: "600",
+    padding : "20px 64px"
+  },
+  MobileChrome: {
+    backgroundColor: "#2B5F47",
+    borderRadius: "800px",
+    color: "#fff",
+    height: "36px",
+    fontSize: "16px",
+    fontWeight: "600",
+    padding : "8px 16px"
+  },
+  
+};
+
+//for medium button
+export const mediumButtonStyles = {
+  chromium: {
+    backgroundColor: "#2B5F47",
+    borderRadius: "800px",
+    color: "#fff",
+    height: "56px",
+    fontSize: "16px",
+    fontWeight: "500",
+    
+  },
+  "chromium-ipad-landscape": {
+    backgroundColor: "#2B5F47",
+    borderRadius: "800px",
+    color: "#fff",
+    height: "56px",
+    fontSize: "16px",
+    fontWeight: "500",
+  },
+  "chromium-ipad-portrait": {
+    backgroundColor: "#2B5F47",
+    borderRadius: "800px",
+    color: "#fff",
+    height: "56px",
+    fontSize: "16px",
+    fontWeight: "500",
+  },
+  MobileChrome: {
+    backgroundColor: "#2B5F47",
+    borderRadius: "800px",
+    color: "#fff",
+    height: "32px",
+    fontSize: "14px",
+    fontWeight: "500",
+  },
+
+}
+
+//light button
+export const lighButtonStyles = {
+  chromium: {
+    backgroundColor: "#EAEFED",
+    borderRadius: "800px",
+    color: "#fff",
+    height: "56px",
+    fontSize: "16px",
+    fontWeight: "500",
+  },
+  "chromium-ipad-landscape": {
+    backgroundColor: "#EAEFED",
+    borderRadius: "800px",
+    color: "#fff",
+    height: "56px",
+    fontSize: "16px",
+    fontWeight: "500",
+  },
+  "chromium-ipad-portrait": {
+    backgroundColor: "#EAEFED",
+    borderRadius: "800px",
+    color: "#fff",
+    height: "56px",
+    fontSize: "16px",
+    fontWeight: "500",
+
+  },
+  MobileChrome: {
+    backgroundColor: "#EAEFED",
+    borderRadius: "800px",
+    color: "#fff",
+    height: "32px",
+    fontSize: "14px",
+    fontWeight: "500",
+  },
+
+}
+
+//border color
+
+export const bordButtonStyles = {
+  chromium: {
+    backgroundColor: "#EAEFED",
+    borderRadius: "800px",
+    color: "#fff",
+    height: "56px",
+    fontSize: "16px",
+    fontWeight: "500" ,
+    
+  },
+  "chromium-ipad-landscape": {
+    backgroundColor: "#EAEFED",
+    borderRadius: "800px",
+    color: "#fff",
+    height: "56px",
+    fontSize: "16px",
+    fontWeight: "500",
+  },
+  "chromium-ipad-portrait": {
+    backgroundColor: "#EAEFED",
+    borderRadius: "800px",
+    color: "#fff",
+    height: "56px",
+    fontSize: "16px",
+    fontWeight: "500",
+
+  },
+  MobileChrome: {
+    backgroundColor: "#EAEFED",
+    borderRadius: "800px",
+    color: "#fff",
+    height: "32px",
+    fontSize: "14px",
+    fontWeight: "500",
+  },
+
+}
+
+//for small button
+export const smallButtonStyles = {
+  chromium: {
+    backgroundColor: "#2B5F47",
+    borderRadius: "800px",
+    color: "#fff",
+    height: "44px",
+    fontSize: "16px",
+    fontWeight: "500",
+    
+  },
+  "chromium-ipad-landscape": {
+    backgroundColor: "#2B5F47",
+    borderRadius: "800px",
+    color: "#fff",
+    height: "44px",
+    fontSize: "16px",
+    fontWeight: "500",
+  },
+  "chromium-ipad-portrait": {
+    backgroundColor: "#2B5F47",
+    borderRadius: "800px",
+    color: "#fff",
+    height: "44px",
+    fontSize: "16px",
+    fontWeight: "500",
+  },
+  MobileChrome: {
+    backgroundColor: "#2B5F47",
+    borderRadius: "800px",
+    color: "#fff",
+    height: "24px",
+    fontSize: "13px",
+    fontWeight: "500",
+  },
+
+}
+
+//light button
+export const LigButtonStyles = {
+  chromium: {
+    backgroundColor: "#EAEFED",
+    borderRadius: "800px",
+    color: "#fff",
+    height: "44px",
+    fontSize: "16px",
+    fontWeight: "500",
+    
+  },
+  "chromium-ipad-landscape": {
+    backgroundColor: "#EAEFED",
+    borderRadius: "800px",
+    color: "#fff",
+    height: "44px",
+    fontSize: "16px",
+    fontWeight: "500",
+  },
+  "chromium-ipad-portrait": {
+    backgroundColor: "#EAEFED",
+    borderRadius: "800px",
+    color: "#fff",
+    height: "44px",
+    fontSize: "16px",
+    fontWeight: "500",
+  },
+  MobileChrome: {
+    backgroundColor: "#EAEFED",
+    borderRadius: "800px",
+    color: "#fff",
+    height: "24px",
+    fontSize: "13px",
+    fontWeight: "500",
+  },
+
+}
+
+//border color
+export const borButtonStyles = {
+  chromium: {
+    backgroundColor: "#2B5F47",
+    borderRadius: "800px",
+    color: "#fff",
+    height: "44px",
+    fontSize: "16px",
+    fontWeight: "500",
+    
+  },
+  "chromium-ipad-landscape": {
+    backgroundColor: "#2B5F47",
+    borderRadius: "800px",
+    color: "#fff",
+    height: "44px",
+    fontSize: "16px",
+    fontWeight: "500",
+  },
+  "chromium-ipad-portrait": {
+    backgroundColor: "#2B5F47",
+    borderRadius: "800px",
+    color: "#fff",
+    height: "44px",
+    fontSize: "16px",
+    fontWeight: "500",
+  },
+  MobileChrome: {
+    backgroundColor: "#2B5F47",
+    borderRadius: "800px",
+    color: "#fff",
+    height: "24px",
+    fontSize: "13px",
+    fontWeight: "500",
+  },
+
+}
+
+
 
 export const textInputFormControlStyles = {
   chromium: {

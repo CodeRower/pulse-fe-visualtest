f259d5d8cb6339a469f54a447252c9fc5f24121c
RanaMahato <rana.mahato@coderower.com>
2023-10-04 11:28:16 +0530

    typography done

--- tests/typography/typography.spec.ts
+++ tests/typography/typography.spec.ts
@@ -5,7 +5,7 @@ import { StandardPageObject } from "../../base/StandardPageObject";
 
 test("Typography Page", async ({ page }) => {
   await page.goto("https://pulse-frontend.web.app/typography");
-
+ 
   // Expect a title "to contain" a substring.
   await expect(page).toHaveScreenshot({ fullPage: true });
 });
@@ -15,4 +15,18 @@ test("Validate Standard Tests", async ({ page }, workerInfo) => {
   const standardPage = new StandardPageObject(page, workerInfo);
   await page.goto("https://pulse-frontend.web.app/typography");
   await standardPage.executeStandardTests();
+});
+
+test('button ', async ({ page }) => {
+  await page.goto('https://pulse-frontend.web.app/typography');
+  await page.getByRole('button', { name: 'Large Filled Button' }).click();
+  await page.getByRole('button', { name: 'Large OutLined Button', exact: true }).click();
+  await page.getByRole('button', { name: 'Large Outlined Button', exact: true }).click();
+  await page.getByRole('button', { name: 'Medium Filled Button' }).click();
+  await page.getByRole('button', { name: 'Medium OutLined Button', exact: true }).click();
+  await page.getByRole('button', { name: 'Medium Outlined Button', exact: true }).click();
+  await page.getByRole('button', { name: 'Small Filled Button' }).click();
+  await page.getByRole('button', { name: 'Small OutLined Button', exact: true }).click();
+  await page.getByRole('button', { name: 'Small Outlined Button', exact: true }).click();
+  await expect(page).toHaveScreenshot({ fullPage: true });
 });

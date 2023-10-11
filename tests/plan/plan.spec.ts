c3d6ca5c9e535e0308fdc67e6fe680ad05aef6a4
Rashida1akter <rashida.akter@coderower.com>
2023-10-11 11:48:26 +0600

    resolve conflicts

--- tests/plan/plan.spec.ts
+++ tests/plan/plan.spec.ts
@@ -4,25 +4,25 @@ import { testUser } from "../../utilities/appConstants";
 
 const pageUrl = "https://pulse-frontend.web.app/plan";
 
-test('Plan', async ({ page }) => {
-  const browser = await chromium.launch()
-const redirect = await browser.newPage()
-  await page.goto('https://pulse-frontend.web.app/');
-  await page.goto('https://pulse-frontend.web.app/join');
-  await page.getByRole('button', { name: 'Login' }).click();
-  await page.getByPlaceholder('Email').click();
-  await page.getByPlaceholder('Email').fill(' rana.mahato@coderower.com');
-  await page.getByPlaceholder('Password').click();
-  await page.getByPlaceholder('Password').fill('RanaMahato@1234');
-  await page.getByRole('button', { name: ' Log in with email' }).click();
- await redirect.goto("https://pulse-frontend.web.app/plan")
- await page.getByLabel("Select option").selectOption("eur");
- await page.getByLabel("Select option").selectOption("usd");
- await page.getByLabel("Select option").selectOption("inr");
- await page.getByRole("button", { name: "Get Started" }).first().click();
- const price = await page.getByRole("heading", { name: "$119 per Month" });
- await expect(price.innerText()).toContain("$119 per Month");
- await page.getByRole("button", { name: "Get Started" }).nth(1).click();
+test("Plan", async ({ page }) => {
+  const browser = await chromium.launch();
+  const redirect = await browser.newPage();
+  await page.goto("https://pulse-frontend.web.app/");
+  await page.goto("https://pulse-frontend.web.app/join");
+  await page.getByRole("button", { name: "Login" }).click();
+  await page.getByPlaceholder("Email").click();
+  await page.getByPlaceholder("Email").fill(" rana.mahato@coderower.com");
+  await page.getByPlaceholder("Password").click();
+  await page.getByPlaceholder("Password").fill("RanaMahato@1234");
+  await page.getByRole("button", { name: " Log in with email" }).click();
+  await redirect.goto("https://pulse-frontend.web.app/plan");
+  await page.getByLabel("Select option").selectOption("eur");
+  await page.getByLabel("Select option").selectOption("usd");
+  await page.getByLabel("Select option").selectOption("inr");
+  await page.getByRole("button", { name: "Get Started" }).first().click();
+  const price = await page.getByRole("heading", { name: "$119 per Month" });
+  await expect(price.innerText()).toContain("$119 per Month");
+  await page.getByRole("button", { name: "Get Started" }).nth(1).click();
   await expect(page).toHaveScreenshot({ fullPage: true });
 });
 

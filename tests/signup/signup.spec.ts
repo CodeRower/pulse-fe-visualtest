240ec4d4746c8c642602b470d52174be1be03c43
Rashida1akter <rashida.akter@coderower.com>
2023-10-13 21:10:37 +0600

    password verification in signup

--- tests/signup/signup.spec.ts
+++ tests/signup/signup.spec.ts
@@ -33,20 +33,61 @@ test("test invalid email", async ({ page }) => {
   await expect(page).toHaveScreenshot({ fullPage: true });
 });
 
-test('test invalid password', async ({ page }) => {
+test("test invalid password", async ({ page }) => {
   await page.goto(pageUrl);
   // await page.locator('.col-6').click();
-  await page.getByPlaceholder('First Name').click();
-  await page.getByPlaceholder('First Name').fill('Rana');
-  await page.getByPlaceholder('Last Name').click();
-  await page.getByPlaceholder('Last Name').fill('Mahato');
-  await page.getByPlaceholder('Email').click();
-  await page.getByPlaceholder('Email').fill('rana@gmail.com');
-  await page.getByPlaceholder('Password').click();
-  await page.getByPlaceholder('Password').fill('12dew');
-  await page.getByPlaceholder('Email').click();
-  await page.getByPlaceholder('Email').fill('rana@gmail.com');
-  await page.getByRole('button', { name: ' Sign Up with email' }).click();
-  
+  await page.getByPlaceholder("First Name").click();
+  await page.getByPlaceholder("First Name").fill("Rana");
+  await page.getByPlaceholder("Last Name").click();
+  await page.getByPlaceholder("Last Name").fill("Mahato");
+  await page.getByPlaceholder("Email").click();
+  await page.getByPlaceholder("Email").fill("rana@gmail.com");
+  await page.getByPlaceholder("Password").click();
+  await page.getByPlaceholder("Password").fill("12dew");
+  await page.getByPlaceholder("Email").click();
+  await page.getByPlaceholder("Email").fill("rana@gmail.com");
+  await page.getByRole("button", { name: " Sign Up with email" }).click();
+
+  await expect(page).toHaveScreenshot({ fullPage: true });
+});
+test("password rules verification", async ({ page }) => {
+  await page.goto(pageUrl);
+  await page.getByPlaceholder("First Name").click();
+  await page.getByPlaceholder("First Name").fill("test");
+  await page.getByPlaceholder("First Name").press("Tab");
+  await page.getByPlaceholder("Last Name").fill("Demo");
+  await page.getByPlaceholder("Last Name").press("Tab");
+  await page.getByPlaceholder("Email").fill("test.demo@gmail.com");
+  await page.getByPlaceholder("Email").press("Tab");
+  await page.getByPlaceholder("Password").click();
+  await page.getByPlaceholder("Password").fill("rashida");
+  await page.getByRole("button", { name: " Sign up with email" }).click();
+  await page.getByPlaceholder("Password").click();
+  await page.getByPlaceholder("Password").fill("123456");
+  await page.getByRole("button", { name: " Sign up with email" }).click();
+  await page.getByRole("button", { name: "" }).click();
+  await page.getByPlaceholder("Password").click();
+  await page.getByPlaceholder("Password").fill("rashida1");
+  await page.getByRole("button", { name: " Sign up with email" }).click();
+  await page.getByRole("button", { name: "" }).click();
+  await page.getByText("8 - 16 Character").click();
+  await page.getByPlaceholder("Password").click();
+  await page.getByRole("button", { name: "" }).click();
+  await page.getByText("At least One Number").click();
+  await page.getByRole("button", { name: "" }).click();
+  await page.getByText("No Spaces").click();
+  await page.getByPlaceholder("Password").click();
+  await page.getByPlaceholder("Password").fill("Rashida1");
+  await page.getByRole("button", { name: "" }).click();
+  await page.getByText("At least One Captial Letter").click();
+  await page.getByPlaceholder("Password").click();
+  await page.getByPlaceholder("Password").fill("Rashida@1");
+  await page.getByPlaceholder("Password").click();
+  await page.getByRole("button", { name: "" }).click();
+  await page
+    .getByLabel("Yes, I want to receive news, updates and all pulse discounts.")
+    .check();
+  await page.getByRole("button", { name: " Sign up with email" }).click();
+
   await expect(page).toHaveScreenshot({ fullPage: true });
 });

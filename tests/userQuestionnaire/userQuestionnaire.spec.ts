5a161054458e33cb59d5aa8405d7b5704d575d08
Rashida1akter <rashida.akter@coderower.com>
2023-10-16 12:54:19 +0600

    add location screen

--- tests/userQuestionnaire/userQuestionnaire.spec.ts
+++ tests/userQuestionnaire/userQuestionnaire.spec.ts
@@ -1,19 +1,22 @@
 import { test, expect, chromium } from "@playwright/test";
 import { StandardPageObject } from "../../base/StandardPageObject";
 import { testUser } from "../../utilities/appConstants";
-
+import { delay } from "../../utilities/utils";
 const pageUrl = "https://pulse-frontend.web.app/location";
 
 test("Location Page", async ({ page }) => {
-  await page.goto("http://localhost:3000/auth/signin");
-  await page.getByPlaceholder("Email").fill("rashida.akter@coderower.com");
-  await page.getByPlaceholder("Password").fill("Rashida@1");
+  await page.goto("https://pulse-frontend.web.app/auth/signin");
+
+  await page.getByPlaceholder("Email").fill(testUser.email);
+  await page.getByPlaceholder("Password").fill(testUser.password);
   await page.getByRole("button", { name: " Log in with email" }).click();
-  await page.goto("http://localhost:3000/location");
+  await delay(2000);
+  await page.goto(pageUrl);
 
   // Expect a title "to contain" a substring.
   await expect(page).toHaveScreenshot({ fullPage: true });
 });
+
 
 // test("Validate Standard Tests", async ({ page }, workerInfo) => {
 //   const standardPage = new StandardPageObject(page, workerInfo);

497b4419bcd4776ea491f10105548191c3aba5c4
Rashida1akter <rashida.akter@coderower.com>
2023-10-23 09:40:58 +0600

    add screens for user profile as users

---
+++ tests/profile/faq&Info.spec.ts
@@ -0,0 +1,24 @@
+import { test, expect } from "@playwright/test";
+import { StandardPageObject } from "../../base/StandardPageObject";
+import { testUser } from "../../utilities/appConstants";
+import { delay } from "../../utilities/utils";
+const pageUrl = "https://pulse-frontend.web.app/faq";
+
+test("Faq Info Page", async ({ page }) => {
+  await page.goto("https://pulse-frontend.web.app/auth/signin");
+
+  await page.getByPlaceholder("Email").fill(testUser.email);
+  await page.getByPlaceholder("Password").fill(testUser.password);
+  await page.getByRole("button", { name: " Log in with email" }).click();
+  await delay(2000);
+  await page.goto(pageUrl);
+
+  // Expect a title "to contain" a substring.
+  await expect(page).toHaveScreenshot({ fullPage: true });
+});
+
+test("Validate Standard Tests", async ({ page }, workerInfo) => {
+  const standardPage = new StandardPageObject(page, workerInfo);
+  await page.goto(pageUrl);
+  await standardPage.executeStandardTests();
+});

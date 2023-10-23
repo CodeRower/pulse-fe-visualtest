757b0db3f1181512c3583daa9c357275ad7fd6fe
Rashida1akter <rashida.akter@coderower.com>
2023-10-23 09:17:14 +0600

    add user questionanire pages

--- tests/userQuestionnaire/Birth.spec.ts
+++ tests/userQuestionnaire/Birth.spec.ts
@@ -2,24 +2,23 @@ import { test, expect, chromium } from "@playwright/test";
 import { StandardPageObject } from "../../base/StandardPageObject";
 import { testUser } from "../../utilities/appConstants";
 import { delay } from "../../utilities/utils";
-const pageUrl = "http://localhost:3000/fitnessGoal";
+const pageUrl = "https://pulse-frontend.web.app/birth";
 
-test("Fitness Goal page", async ({ page }) => {
-  await page.goto("http://localhost:3000/auth/signin");
+test("Birth Page", async ({ page }) => {
+  await page.goto("https://pulse-frontend.web.app/auth/signin");
 
   await page.getByPlaceholder("Email").fill(testUser.email);
   await page.getByPlaceholder("Password").fill(testUser.password);
   await page.getByRole("button", { name: " Log in with email" }).click();
-  await delay(10000);
-//   await page.goto("https://pulse-frontend.web.app/location");
+  await delay(2000);
   await page.goto(pageUrl);
 
   // Expect a title "to contain" a substring.
   await expect(page).toHaveScreenshot({ fullPage: true });
 });
 
-// test("Validate Standard Tests", async ({ page }, workerInfo) => {
-//   const standardPage = new StandardPageObject(page, workerInfo);
-//   await page.goto(pageUrl);
-//   await standardPage.executeStandardTests();
-// });
+test("Validate Standard Tests", async ({ page }, workerInfo) => {
+  const standardPage = new StandardPageObject(page, workerInfo);
+  await page.goto(pageUrl);
+  await standardPage.executeStandardTests();
+});
---
+++ tests/userQuestionnaire/HealthCondition.spec.ts
@@ -0,0 +1,24 @@
+import { test, expect } from "@playwright/test";
+import { StandardPageObject } from "../../base/StandardPageObject";
+import { testUser } from "../../utilities/appConstants";
+
+const pageUrl = "https://pulse-frontend.web.app/healthCondition";
+
+test("Health Condition Page", async ({ page }) => {
+  await page.goto("https://pulse-frontend.web.app/auth/signin");
+
+  await page.getByPlaceholder("Email").fill(testUser.email);
+  await page.getByPlaceholder("Password").fill(testUser.password);
+  await page.getByRole("button", { name: " Log in with email" }).click();
+
+  await page.goto(pageUrl);
+
+  // Expect a title "to contain" a substring.
+  await expect(page).toHaveScreenshot({ fullPage: true });
+});
+
+test("Validate Standard Tests", async ({ page }, workerInfo) => {
+  const standardPage = new StandardPageObject(page, workerInfo);
+  await page.goto(pageUrl);
+  await standardPage.executeStandardTests();
+});
